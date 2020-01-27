a6bf03191c20dc0a238c92f5f3d3337dd2ae0c61
Julian Krumow <julian.krumow@bogusmachine.com>
2020-01-27 21:04:13 +0100

    Add M2.

---
+++ M2/cog1/modelData/plainTriangles.js
@@ -0,0 +1,78 @@
+/**
+ * Empty 3D Data Store for a model.
+ * 
+ * @namespace cog1.data
+ * @module plain
+ */
+define(["exports", "data"], function (exports, data) {
+    "use strict";
+
+	/**
+	 * Create an instance of the model defined in this module.
+	 * 
+	 * @parameter object with fields:
+	 * @parameter scale is the edge length of the cube.
+	 * @parameter color 
+	 * 
+	 * @returns instance of this model.
+	 */
+    exports.create = function (parameter) {
+
+        if (parameter) {
+            var scale = parameter.scale;
+            var color = parameter.color;
+            var textureURL = parameter.textureURL;
+        }
+        // Set default values if parameter is undefined.
+        if (scale == undefined) {
+            scale = 200;
+        }
+        if (color == undefined) {
+            color = 9;
+        }
+        if (textureURL == undefined) {
+            textureURL = "";
+        }
+
+        // Instance of the model to be returned.
+        var instance = {};
+
+        // Vertex indices:							
+        // 4----3
+        // |  2 |
+        // 0----1
+        instance.vertices = [
+            // x-y plain (z=0)
+            [-1, -1, 0],
+            [1, -1, 0],
+            [0, 0, 0],
+            [1, 1, 0],
+            [-1, 1, 0],
+        ];
+        // Use default colors, implicitly.
+        // instance.colors = data.colors;
+
+        // Corners of the faces have to fit the texture coordinates.			
+        // Faces: bottom/down, top/up, front, right, back, left. 
+        instance.polygonVertices = [
+            [0, 1, 2],
+            [1, 3, 2],
+            [3, 4, 2],
+            [0, 2, 4]
+        ];
+
+        instance.polygonColors = [color, color, color, color];
+
+        //instance.vertexNormals = [];
+        //instance.polygonNormals = [];
+
+        // Use default texture coordinates.
+        instance.textureCoord = [];
+
+        instance.textureURL = textureURL;
+
+        data.applyScale.call(instance, scale);
+
+        return instance;
+    }
+});
